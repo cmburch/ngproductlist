daebeb4a66986534e785a8c1d7a88a2a1a52064f
Charles Burch <charlesburch@CBMacBook-Pro.local>
2017-06-30 20:13:43 -0500

    first commit to angular app

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -24,13 +24,13 @@ export class ProductListComponent implements OnInit {
     filteredProducts: IProduct[];
 
     // This is just a test code the best practice is to put the data into a service.
-    // This will be converted to service in the future
+    // This will be converted to service in the future.This is a demo app for filtering list of data
     products: IProduct[] = [
     {
         "productId": 1,
         "productName": "Leaf Rake",
         "productCode": "GDN-0011",
-        "releaseDate": "future",
+        "releaseDate": "july",
         "description": "Leaf rake with 48-inch wooden handle.",
         "price": 19.95,
         "starRating": 3.2,
@@ -40,7 +40,7 @@ export class ProductListComponent implements OnInit {
         "productId": 2,
         "productName": "Garden Cart",
         "productCode": "GDN-0023",
-        "releaseDate": "future",
+        "releaseDate": "april",
         "description": "15 gallon capacity rolling garden cart",
         "price": 32.99,
         "starRating": 4.2,
@@ -50,7 +50,7 @@ export class ProductListComponent implements OnInit {
         "productId": 5,
         "productName": "Hammer",
         "productCode": "TBX-0048",
-        "releaseDate": "future",
+        "releaseDate": "march",
         "description": "Curved claw steel hammer",
         "price": 8.9,
         "starRating": 4.8,
@@ -75,12 +75,41 @@ export class ProductListComponent implements OnInit {
         "price": 35.95,
         "starRating": 4.6,
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+    },  {
+        "productId": 15,
+        "productName": "Sports Car",
+        "productCode": "EBX-0095",
+        "releaseDate": "future",
+        "description": "The best car money can buy",
+        "price": 1.90,
+        "starRating": 4.8,
+        "imageUrl": "https://openclipart.org/image/90px/svg_to_png/118531/1297818301.png"
+    },
+    {
+        "productId": 22,
+        "productName": "tv",
+        "productCode": "TBX-0022",
+        "releaseDate": "future",
+        "description": "15-inch old tv",
+        "price": 11.55,
+        "starRating": 4.7,
+        "imageUrl": "https://openclipart.org/image/90px/svg_to_png/11295/Machovka-TV-set-2.png"
+    },
+    {
+        "productId": 14,
+        "productName": "Hamburger",
+        "productCode": "GMG-0042",
+        "releaseDate": "future",
+        "description": "Hamburger that taste good",
+        "price": 35.95,
+        "starRating": 4.6,
+        "imageUrl": "https://openclipart.org/image/90px/svg_to_png/280262/gramzon-Hamburger-remix-monster-brain.png"
     }
 ];
 
     constructor() {
         this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+        this.listFilter = '';
     }
 
     performFilter(filterBy: string): IProduct[] {
